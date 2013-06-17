e4bc17fd7f99cb4da9380b646a6bfe656f97b497
Clifford Heath <clifford.heath@gmail.com>
2013-06-17 12:03:05 +1000

    Added metaprogramming skeleton

--- lib/facetious.rb
+++ lib/facetious.rb
@@ -2,4 +2,23 @@
 require 'active_record'
 
 module Facetious #:nodoc:
+  # These methods are made available on all ActiveRecord classes:
+  module ClassMethods
+    def facet *args
+      extend ClassPrivate
+      include Facetious
+    end
+  end
+
+  # These methods are made available on faceted ActiveRecord classes:
+  module ClassPrivate
+    def some_faceted_class_method
+    end
+  private
+    # def some_faceted_class_private_method; end
+  end
+
+  # These methods will be publicly visible on faceted instances:
+  def some_faceted_instance_method
+  end
 end
